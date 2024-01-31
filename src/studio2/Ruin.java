db8eb2f9cfc95c6641a5b9570cb4148eefbb9419
xiomaracolopy <xiomaracolopy@localhost>
2024-01-31 14:18:30 -0600

    Ok actually finished now

--- src/studio2/Ruin.java
+++ src/studio2/Ruin.java
@@ -13,18 +13,21 @@ public class Ruin {
 		double money = startAmount;
 		
 		for(int i=1; i <=totalSimulations; i+=1) {
+			
 			money=startAmount;
+			tries = 0;
+			
 			while ( money > 0 && money < winLimit) {
 			
 				if(Math.random() > winChance) {
 					money -= 1;
-					System.out.println("loss");
+					//System.out.println("loss");
 					tries += 1;	
 				
 				}
 				else {
 					money += 1;
-					System.out.println("win");
+					//System.out.println("win");
 					tries += 1;
 				}
 
@@ -32,14 +35,14 @@ public class Ruin {
 
 		}
 		if(money == 0) {
-			System.out.println("FAILURE");
+			System.out.println("Similuatrion " + i + ": FAIL");
 		}
 		else {
-			System.out.println("SUCCESS");
+			System.out.println("Simulation " + i + ": SUCCESS");
 		}
 
-		System.out.println(tries);
-		System.out.println(i);
+		System.out.println("Tries: " + tries);
+		//System.out.println(i);
 		
 		}
 	}
